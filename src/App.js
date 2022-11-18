977ab0229598cb10917652017fe50b7dc4b2a82b
murilodsd <mdsouzadias@hotmail.com>
2022-11-18 15:59:59 -0300

    folders created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Properties from './pages/Properties';
 
 function App() {
   return (
     <div className="App">
-     
+     <Route>
+      <Routes path='/' element = { <Properties /> } />
+     </Route>
     </div>
   );
 }
---
+++ src/pages/Properties.js
@@ -0,0 +1,9 @@
+function Properties() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Properties;
