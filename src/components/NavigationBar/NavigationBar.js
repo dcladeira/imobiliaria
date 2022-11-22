f092dce1b1711824c9c9cbcef47e972cb78ef32f
dcladeira <daniel.ladeira@hotmail.com>
2022-11-22 16:27:46 -0300

    NavigationBar atualizada para funcionar com o AddProperties estilo modal

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,20 +1,30 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import AddProperties from "../Properties/AddProperties";
+import { Link } from "react-router-dom";
 
-function NavigationBar({apiURL, body, setBody}) {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand>Iron Imóveis</Navbar.Brand>
-        <Nav className="me-auto">
-          {/* <Link className="nav-link" to={"/cadastrar"}>Cadastrar</Link> */}
-          <AddProperties apiURL={apiURL} body={body} setBody={setBody} />
-          <Link className="nav-link" to={"/"}>Voltar</Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-}
+function NavigationBar({isAdmin, showLogin, setShowLogin, apiUrl}) {
+    function handleClick() {
+        setShowLogin(true);
+        console.log(showLogin);
+    }
+
+    return (
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>Iron Imobiliária</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link className='nav-link' to="/">Home</Link>
+              <Link className='nav-link' to="/Contato">Contato</Link>
+              {/* {isAdmin && <Link className='nav-link' to='/add-properties'>Adicionar imóvel</Link>} */}
+              {isAdmin && <AddProperties apiUrl={apiUrl} />}
+            </Nav>
+            <Button onClick={handleClick} as="input" type="button" value="Logar" />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+};
 
 export default NavigationBar;
