932d3f1c31196824f1be937ca64b249d5b48500c
dcladeira <daniel.ladeira@hotmail.com>
2022-11-23 08:22:29 -0300

    Componente ProopertiesListe para teste.

--- src/components/Properties/PropertiesList.js
+++ src/components/Properties/PropertiesList.js
@@ -1,9 +1,41 @@
-function Properties() {
-    return (
-        <div>
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-        </div>
-    )
+function PropertiesList({ apiUrl }) {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    try {
+      const fetchProperties = async () => {
+        const response = await axios.get(apiUrl);
+        setProperties(response.data);
+      };
+      fetchProperties();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [apiUrl]);
+
+  return (
+    <Container>
+      {properties.map((property) => {
+        return (
+          <Row key={property._id}>
+            <Col>{property._id}</Col>
+            <Col>
+              <Button variant="primary" size="sm">
+                <Link className="nav-link" to={`/properties/${property._id}`}>
+                  Detalhes
+                </Link>
+              </Button>
+            </Col>
+          </Row>
+        );
+      })}
+    </Container>
+  );
 }
 
-export default Properties;
+export default PropertiesList;
